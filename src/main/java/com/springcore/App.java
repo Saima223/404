1e402633fc37efe04ec98223ec34bc077668bebe
Saima223 <siddiqui22062004@gmail.com>
2025-07-07 22:27:00 +0530

    Create config file

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -1,13 +1,27 @@
 package com.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        // Load Spring context
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        
+        // Get student bean from context
+        Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+        
+        System.out.println(student1);
+        System.out.println(student2);
+        
+        // Close context (if using ClassPathXmlApplicationContext)
+        ((ClassPathXmlApplicationContext) context).close();
     }
 }
