17700e37edccee25eb64c65532aa994a3d5376d0
Saima223 <siddiqui22062004@gmail.com>
2026-02-27 23:28:04 +0530

    revise the variables and its type

--- src/main/java/com/javacore/VarTypes.java
+++ src/main/java/com/javacore/VarTypes.java
@@ -1,14 +1,14 @@
 package com.javacore;
 
 public class VarTypes {
-	// Instance variable
-    int instanceVar = 10;
+	// Instance variable: inside a class and outside any method, they are not shared between objs 
+    int instanceVar = 10;// each obj have its own version of them(unique)
 
-    // Static variable
+    // Static variable: belongs to class, dont need any obj (shared)
     static int staticVar = 20;
 
     public void methodExample() {
-        // Local variable
+        // Local variable: it can be created inside a method and they exist within that scope  
         int localVar = 5;
         System.out.println("Local Variable: " + localVar);
         System.out.println("Instance Variable: " + instanceVar);
